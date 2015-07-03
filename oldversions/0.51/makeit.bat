a698e78f7c7c1857b2b9145d1562c1d8d2177634
nitrofurano <nitrofurano@gmail.com>
2015-07-03 22:09:25 +0100

    Create makeit.bat

---
+++ oldversions/0.51/makeit.bat
@@ -0,0 +1,12 @@
+del y_tab.c
+' for checking of the grammar has problems
+' redir -eo bison -d -y wxbasic.y > err
+' edit err
+
+' when there are no grammar problems
+bison -d -y wxbasic.y
+
+rename y.tab.c y_tab.c
+
+redir -eo gcc core.c -owxbasic.exe > err
+edit err
