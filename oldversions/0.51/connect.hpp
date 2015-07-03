bfe8b464c0d2df889e1106b456f4e8aca7a09085
nitrofurano <nitrofurano@gmail.com>
2015-07-03 21:33:03 +0100

    Create connect.hpp

---
+++ oldversions/0.51/connect.hpp
@@ -0,0 +1,10 @@
+/*
+    Name:       connect.hpp
+    Purpose:    Callbacks for events in wxBasic
+    Author:     David Cuny
+    Copyright:  (c) 2002 David Cuny <dcuny@lanset.com>
+    Licence:    LGPL
+*/
+
+void wConnect( int id, int useId, int handle, int theEvent, wSymbol *routine );
+void wRunCallback(wxEvent *event);
