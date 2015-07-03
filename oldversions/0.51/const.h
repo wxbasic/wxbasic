4f0d38b08f4cc5c4247d6770031e21eb609cd6b4
nitrofurano <nitrofurano@gmail.com>
2015-07-03 21:34:32 +0100

    Create const.h

---
+++ oldversions/0.51/const.h
@@ -0,0 +1,10 @@
+/*
+    Name:       const.h
+    Purpose:    Builtin constants for wxBasic
+    Author:     David Cuny
+    Copyright:  (c) 2002 David Cuny <dcuny@lanset.com>
+    Licence:    LGPL
+*/
+
+void wCreatePtrConst( char *name, int classIndex, int pointer );
+void wInitConsts( void );
